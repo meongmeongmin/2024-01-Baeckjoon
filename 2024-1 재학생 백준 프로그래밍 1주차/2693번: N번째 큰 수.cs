b7a2e867cdf837358b67073c6ca71cce3f930b4c
멍멍민 <118867497+meongmeongmin@users.noreply.github.com>
2024-03-26 15:24:59 +0900

    Update 2693번: N번째 큰 수.cs

--- 2024-1 재학생 백준 프로그래밍 1주차/2693번: N번째 큰 수.cs
+++ 2024-1 재학생 백준 프로그래밍 1주차/2693번: N번째 큰 수.cs
@@ -13,19 +13,19 @@ public class Program
         using var read = new System.IO.StreamReader(Console.OpenStandardInput());
         using var print = new System.IO.StreamWriter(Console.OpenStandardOutput());
         #endregion
-        
+
         int input = Convert.ToInt32(read.ReadLine());   // 입력
         int[][] num = new int[input][];
 
-	    string inputNum;
-        for (int i = 0; i < input; i++) 
+        string inputNum;
+        for (int i = 0; i < input; i++)
         {
             inputNum = read.ReadLine();
             num[i] = Array.ConvertAll(inputNum.Split(), int.Parse);
             Array.Sort(num[i]);
         }
 
-        for (int i = 0; i < input; i++) 
+        for (int i = 0; i < input; i++)
         {
             print.WriteLine(num[i][7]);
         }
